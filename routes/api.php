ee106ed0191590ceee99613a7211b41d1683ae28
enho.borges.rocha@gmail.com <enho@Enho-PC.>
2025-03-19 22:33:08 -0300

    Primeiro commit com alguns arquivos prontos (model e migration)

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/teste', function () {
+    return response()->json([
+        'teste' => 1
+    ]);
+});
+
+Route::post('/products', function () {
+    
+});
